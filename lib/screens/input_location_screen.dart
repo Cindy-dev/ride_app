e5215449515ebfd709be33ddde2da466a7c6f848
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2021-03-01 14:38:34 +0100

    location

--- lib/screens/input_location_screen.dart
+++ lib/screens/input_location_screen.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
+import 'package:bolt/providers/app_data.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:google_maps_webservice/places.dart';
-import '../providers/app_data.dart';
+import 'package:provider/provider.dart';
 import '../helpers/location_helpers.dart';
 import '../helpers/http_request.dart';
 import '../screens/screens1.dart';
@@ -24,7 +24,7 @@ GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: GOOGLE_API_KEY);
 class _InputLocationState extends State<InputLocation> {
   Completer<GoogleMapController> _controllerGoogleMap = Completer();
   final pickUpTEC = TextEditingController();
-  final destinationTEC = TextEditingController();
+  final destinationTEC = TextEditingController();   
   Position currentPosition;
   final geoLocator = Geolocator();
   double bottomPaddingOfMap = 0;
@@ -44,7 +44,12 @@ class _InputLocationState extends State<InputLocation> {
     homeScaffoldKey.currentState
         .showSnackBar(SnackBar(content: Text(response.errorMessage)));
   }
-
+  placeAdress(){ 
+    setState(() {
+        String placeAddress = Provider.of<AppData>(context).pickUpLocation.placeName ?? "";
+        pickUpTEC.text = placeAddress;
+    });
+  }
   Mode _mode = Mode.overlay;
   @override
   Widget build(BuildContext context) {
@@ -87,7 +92,7 @@ class _InputLocationState extends State<InputLocation> {
                 child: Column(
                   children: [
                     Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      padding: const EdgeInsets.fromLTRB(40,18,45,0), 
                       child: TextField(
                           cursorHeight: 25,
                           cursorColor: Colors.green.shade400,
@@ -107,85 +112,103 @@ class _InputLocationState extends State<InputLocation> {
                             border: InputBorder.none,
                             fillColor: Colors.white12,
                           ),
-                          controller: pickUpTextEditingController),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
-                      child: ListTile(
-                        leading: Icon(
-                          Icons.location_on,
-                          color: Colors.deepPurple.shade700,
-                          size: 20,
+                          controller: pickUpTEC,
                         ),
-                        title: GestureDetector(
-                          onTap: () => _handlePressButton(),
-                          child: AbsorbPointer(
-                            child: TextField(
-                              cursorHeight: 25,
-                              cursorColor: Colors.green.shade400,
-                              decoration: InputDecoration(
-                                hintText: 'Enter destination',
-                                hintStyle:
-                                    TextStyle(fontWeight: FontWeight.w300),
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                  color: Colors.black26,
-                                )),
-                                enabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                  width: 2,
-                                  color: Colors.black26,
-                                )),
-                                border: InputBorder.none,
-                                filled: true,
-                                fillColor: Colors.white24,
+                    ),
+                     Row(children:[
+                        Flexible(
+                          flex: 1,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(5,18,0,0),
+                            child: Icon(
+                              Icons.location_on,
+                              color: Colors.black54,
+                              size: 20,
+                            ),
+                          ),
+                        ),  
+                        Flexible(
+                          flex: 10,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(15,18,0,0),
+                            child: GestureDetector(
+                              onTap: () => _handlePressButton(),
+                              child: AbsorbPointer(
+                                child: TextField(
+                                  cursorHeight: 25,
+                                  cursorColor: Colors.green.shade400,
+                                  decoration: InputDecoration(
+                                    hintText: 'Enter destination',
+                                    hintStyle:
+                                        TextStyle(fontWeight: FontWeight.w300),
+                                    focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                      color: Colors.black26,
+                                    )),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                      width: 2,
+                                      color: Colors.black26,
+                                    )),
+                                    border: InputBorder.none,
+                                    filled: true,
+                                    fillColor: Colors.white24,
+                                  ),
+                                  controller: destinationTEC,
+                                  onChanged: (val) {
+                                    findPlace(val);
+                                  },
+                                ),
                               ),
-                              controller: destinationTextEditingController,
-                              onChanged: (val) {
-                                findPlace(val);
-                              },
                             ),
                           ),
                         ),
-                        trailing: GestureDetector(
-                          child: Icon(Icons.add),
+                        Flexible(
+                          flex: 1,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(5,18,5,0),
+                            child: GestureDetector(
+                              child: Icon(Icons.add, size: 30,),
+                            ),
+                          ),
                         ),
-                      ),
+                      ]
                     ),
                   ],
                 ),
               ),
-              Provider.of<AppData>(context).pickUpLocation != null
-                  ? Provider.of<AppData>(context).pickUpLocation.placeName
-                  : Card(
-                      elevation: 0.2,
-                      child: ListTile(
-                          leading: Icon(Icons.home),
-                          title: Text('Enter home location')),
-                    ),
-              Card(
-                elevation: 0.2,
-                child: ListTile(
-                    leading: Icon(Icons.home_repair_service_outlined),
-                    title: Text('Enter work location')),
-              ),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.location_searching,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text('Choose on map'),
-                      ),
-                    ],
-                  ),
-                ),
-              )
+              // Provider.of<AppData>(context).pickUpLocation != null
+              //     ? Provider.of<AppData>(context).pickUpLocation.placeName
+              //     : Card(
+              //         elevation: 0.2,
+              //         child: ListTile(
+              //             leading: Icon(Icons.home),
+              //             title: Text('Enter home location')),
+              //       ),
+              // Card(
+              //   elevation: 0.2,
+              //   child: ListTile(
+              //       leading: Icon(Icons.home_repair_service_outlined),
+              //       title: Text('Enter work location')),
+              // ),
+              // SpacingHeight(530),
+              // Card(
+              //   child: Padding(
+              //     padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+              //     child: Row(
+              //       mainAxisAlignment: MainAxisAlignment.center,
+              //       children: [
+              //         Icon(
+              //           Icons.location_searching,
+              //         ),
+              //         Padding(
+              //           padding: const EdgeInsets.all(8.0),
+              //           child: Text('Choose on map'),
+              //         ),
+              //       ],
+              //     ),
+              //   ),
+              // )
             ],
           ),
         ),
